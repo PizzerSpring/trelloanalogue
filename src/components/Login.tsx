0df79f8143e8f35e4da724868215d3d820558fdf
PizzerSpring <vladi-slav96@mail.ru>
2022-12-10 19:00:24 +0700

    starting login styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const LoginContainer = styled.div`
+`
+const bgLeft = styled.div`
+`
+const Form = styled.form`
+`
+const Title = styled.h1`
+`
+const TextTitle = styled.h2`
+`
+const Email = styled.input`
+`
+const Password = styled.input`
+`
+const Button = styled.a`
+`
+const Icon = styled.span`
+`
+const bgRight = styled.div`
+`
+const footerContainer = styled.div`
+`
+const footer = styled.footer`
+`
 
 const Login = () => {
     return (
-        <div>
+        <LoginContainer>
 
-        </div>
+        </LoginContainer>
     );
 };
 
